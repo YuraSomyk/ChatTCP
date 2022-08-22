3a86efe63b25ad25e0441886b3635e0de5f4f48e
Юра Сомик <yuriy.somyk@outlook.com>
2022-08-22 09:45:23 +0300

    Init Chat

--- ChatTCP/Program.cs
+++ ChatTCP/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace ChatTCP
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ChatTCP.Server {
+
+    class Program {
+        static Server server; // сервер
+        static Thread listenThread; // потока для прослушивания
+
+        static void Main(string[] args) {
+            try {
+                server = new Server();
+                listenThread = new Thread(new ThreadStart(server.Listen));
+                listenThread.Start(); //старт потока
+            } catch (Exception ex) {
+                server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
